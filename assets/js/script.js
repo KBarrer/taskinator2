3659919a8dbd528fe4dfdeade68f1ed9b6a16317
KB <78621363+KBarrer@users.noreply.github.com>
2024-04-09 09:24:32 -0600

    form resets

--- assets/js/script.js
+++ assets/js/script.js
@@ -7,6 +7,14 @@ var taskFormHandler = function(event) {
     var taskNameInput = document.querySelector("input[name='task-name']").value;
     var taskTypeInput = document.querySelector("select[name='task-type']").value;
     
+    // check if input vales are empty strings
+    if (!taskNameInput || !taskTypeInput) {
+        alert("You need to fill out the task form!");
+        return false;
+    }
+
+    formEl.reset();
+    
     // package up data as an object
     var taskDataObj = {
         name: taskNameInput,
@@ -24,7 +32,7 @@ var createTaskEl = function(taskDataObj) {
 
     // create div to hold task info and add to list item
     var taskInfoEl = document.createElement("div");
-    // give it a clas name
+    // give it a class name
     taskInfoEl.className = "task-info";
     // add html content to div
     taskInfoEl.innerHTML = "<h3 class='task-name'>" + taskDataObj.name + "</h3><span class='task-type'>" + taskDataObj.type + "</span>";
